60f26f9ae036dbeb4066a0e522af81004837a080
EC2 Default User <ec2-user@ip-172-31-33-89.us-east-2.compute.internal>
2019-04-15 08:02:49 +0000

    layouts

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
+
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
